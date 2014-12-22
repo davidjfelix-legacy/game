28e47634ea9be250b0d0d6581f144c88f286c7a2
David J. Felix <felix.davidj@gmail.com>
2014-12-22 14:03:49 -0500

    Update main.py

--- src/client/main.py
+++ src/client/main.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 
+import signal
+import pyuv as uv
+
 from __future__ import print_function
+
+def on_read(handle, ip_port, flags, data, error):
+	if data is not None:
+		handle.send(ip_port, data)
+
+def signal_cb():
+	pass
+
 
 def main():
 	print("Running main loop")
+	
+	loop = uv.loop.default_loop()
+	responder = pyuv.UDP(loop)
+	responder.bind(("0.0.0.0", 1234))
+	responder.start_recv(on_read)
+	
+	signal_h = pyuv.Signal(loop)
+	signal_h.start(signal_cb, signal.SIGINT)
+	
+	loop.run()
+	
+	print("Ending main loop")
+
 
 if __name__ == "__main__":
 	print("Hello Client!")
