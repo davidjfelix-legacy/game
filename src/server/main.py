1c980a3d9d550b2ef413df03118fcc0b61f2ae3f
David J. Felix <felix.davidj@gmail.com>
2014-12-23 23:34:12 -0500

    Update main.py

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+import signal
+import pyuv as uv
+
+from __future__ import print_function
+
+def on_read(handle, ip_port, flags, data, error):
+	if data is not None:
+		handle.send(ip_port, data)
+
+def signal_cb():
+	pass
+
 from __future__ import print_function
 
 def main():
