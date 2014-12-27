05e918471a809be9f25886f3a51ca284b884b8e7
David J. Felix <felix.davidj@gmail.com>
2014-12-27 12:42:41 -0500

    Classify client

--- src/client/main.py
+++ src/client/main.py
@@ -5,26 +5,29 @@ import pyuv as uv
 
 from __future__ import print_function
 
-def on_read(handle, ip_port, flags, data, error):
-	if data is not None:
-		handle.send(ip_port, data)
+class Client(object):
+	
+	def __init__(self, loop):
+		self.udp_responder = pyuv.UDP(loop)
+		self.udp_responder.bind(("0.0.0.0", 1234))
+		self.udp_responder.start_recv(self.on_read)
+		self.signal_h = pyuv.Signal(loop)
+		self.signal_h.start(self.signal_cb, signal.SIGINT)
+		
+	def on_read(handle, ip_port, flags, data, error):
+		if data is not None:
+			handle.send(ip_port, data)
 
-def signal_cb(handle, signum):
-	signal_h.close()
-	responder.close()
+	def signal_cb(handle, signum):
+		self.signal_h.close()
+		self.udp_responder.close()
 
 
 def main():
 	print("Running main loop")
 	
 	loop = uv.loop.default_loop()
-	responder = pyuv.UDP(loop)
-	responder.bind(("0.0.0.0", 1234))
-	responder.start_recv(on_read)
-	
-	signal_h = pyuv.Signal(loop)
-	signal_h.start(signal_cb, signal.SIGINT)
-	
+	client = Client(loop)
 	loop.run()
 	
 	print("Ending main loop")
