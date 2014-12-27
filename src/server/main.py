f8be3f09bc5cbc1a2b67870bf61a1677a8b5f62a
David J. Felix <felix.davidj@gmail.com>
2014-12-27 11:33:31 -0500

    Classify server

--- src/server/main.py
+++ src/server/main.py
@@ -5,27 +5,30 @@ import pyuv as uv
 
 from __future__ import print_function
 
-def on_read(handle, ip_port, flags, data, error):
-	if data is not None:
-		handle.send(ip_port, data)
+class Server(object):
 
-def signal_cb(handle, signum):
-	signal_h.close()
-	server.close()
+	def __init__(self, loop):
+		self.udp_server = pyuv.UDP(loop)
+		self.udp_server.bind(("0.0.0.0", 1235))
+		self.udp_server.start_recv(self.on_read)
+		
+		self.signal_h = pyuv.Signal(loop)
+		self.signal_h.start(self.signal_cb, signal.SIGINT)
 
-from __future__ import print_function
+	def on_read(handle, ip_port, flags, data, error):
+		if data is not None:
+			handle.send(ip_port, data)
+
+	def signal_cb(handle, signum):
+		signal_h.close()
+		server.close()
+
 
 def main():
 	print("Running Main Loop!")
 	
 	loop = uv.loop.default_loop()
-	server = pyuv.UDP(loop)
-	server.bind(("0.0.0.0", 1235))
-	server.start_recv(on_read)
-	
-	signal_h = pyuv.Signal(loop)
-	signal_h.start(signal_cb, signal.SIGINT)
-	
+	server = Server(loop)
 	loop.run()
 	
 	print("Ending main loop")
