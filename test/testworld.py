26e8c1f51d65abc1b832b1ba919b1c87efd298bf
David J Felix <felix.davidj@gmail.com>
2015-01-06 19:33:07 +0000

    Add testing framework

--- test/testworld.py
+++ test/testworld.py
@@ -1,6 +1,6 @@
 import unittest
-from game.world import World
-from game.player import Player
+from game.shared.world import World
+from game.shared.player import Player
 
 class TestWorld(unittest.TestCase):
 	
@@ -11,3 +11,7 @@ class TestWorld(unittest.TestCase):
 		player = Player()
 		self.world.add_player(player)
 		assertTrue(player in self.world.players)
+
+if __name__ == "__main__":
+	unittest.main()
+
