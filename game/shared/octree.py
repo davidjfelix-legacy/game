bdd826bfc1417993942035b425ec4580cc7b3f17
David J Felix <felix.davidj@gmail.com>
2015-01-12 22:14:07 +0000

    Refactor octree to shut pylint up

--- game/shared/octree.py
+++ game/shared/octree.py
@@ -3,9 +3,13 @@
 
 class OctreeNode(object):
 
-    def __init__(self, data=None, children=[], parent=None):
+    def __init__(self, data=None, children=None, parent=None):
+        if children is None:
+            self.children = []
+        else:
+            self.children = children
+
         self.data = data
-        self.children = children
         self._parent = parent
 
     @property
@@ -24,14 +28,14 @@ class OctreeNode(object):
         self._parent = None
 
 
-def Octree(OctreeNode):
+class Octree(OctreeNode):
 
     def get_child_with_index(self, index):
-        return children[index]
+        return self.children[index]
 
-    def get_child_with_coords(self, x, y, z):
-        index = int("0o" + str(x) + str(y) + str(z), base=8)
+    def get_child_with_coords(self, x_coord, y_coord, z_coord):
+        index = int("0o" + str(x_coord) + str(y_coord) + str(z_coord), base=8)
         return self.get_child_with_index(index)
 
-    def get_deep_child_with_coords(self, x, y, z):
+    def get_deep_child_with_coords(self, x_coord, y_coord, z_coord):
         pass
