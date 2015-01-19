0993a8475339741ff9d47380b4f65d16c36e7a46
David J Felix <felix.davidj@gmail.com>
2015-01-19 04:12:04 +0000

    Add some option code for address

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 mod chunk {
 
+    use std::fmt;
     struct Address(u64, u64, u64);
 
     struct Chunk {
@@ -11,33 +12,29 @@ mod chunk {
     }
 
     impl Address {
-        fn from_string(address: String) -> Address {
-            let mut x: u64 = 1;
-            let mut y: u64 = 1;
-            let mut z: u64 = 1;
-            let mut xadd: u8 = 0;
-            let mut yadd: u8 = 0;
-            let mut zadd: u8 = 0;
+        fn from_string(address: String) -> Option<Address> {
+            let mut xyz: (u64, u64, u64) = (1, 1, 1);
+            let mut xyz_add: (u64, u64, u64);
 
-            for (i, c) in address.split_str("").enumerate() {
+            for (i, c) in address.chars().enumerate() {
                 if i > 63 {
                     break;
                 }
-                let (xadd, yadd, zadd) = match c {
-                    "0" => (0, 0, 0),
-                    "1" => (0, 0, 1),
-                    "2" => (0, 1, 0),
-                    "3" => (0, 1, 1),
-                    "4" => (1, 0, 0),
-                    "5" => (1, 0, 1),
-                    "6" => (1, 1, 0),
-                    "7" => (1, 1, 1),
-                    _ => (0,0,0),
+                xyz_add =  match c.to_digit(8){
+                    Some(0) => (0, 0, 0),
+                    Some(1) => (0, 0, 1),
+                    Some(2) => (0, 1, 0),
+                    Some(3) => (0, 1, 1),
+                    Some(4) => (1, 0, 0),
+                    Some(5) => (1, 0, 1),
+                    Some(6) => (1, 1, 0),
+                    Some(7) => (1, 1, 1),
+                    _ => return None,
                 };
-                (x, y, z) = (x << 1, y << 1, z << 1)
-                (x, y, z) = (x + xadd, y + yadd, z + zadd)
+                xyz = (xyz.0 << 1, xyz.1 << 1, xyz.2 << 1);
+                xyz = (xyz.0 + xyz_add.0, xyz.1 + xyz_add.1, xyz.2 + xyz_add.2);
             }
-            Address(x, y, z)
+            Some(Address(xyz.0, xyz.1, xyz.2))
         }
     }
 
@@ -45,6 +42,23 @@ mod chunk {
         fn to_string(&self) -> String {
             String::from_str("")
         }
-        
+    }
+
+    impl PartialEq for Address {
+        fn eq(&self, other: &Self) -> bool {
+            (((self.0 == other.0) && (self.1 == other.1)) && (self.2 == other.2))
+        }
+    }
+
+    impl fmt::Show for Address {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Address({}, {}, {})", self.0, self.1, self.2)
+        }
+    }
+
+    #[test]
+    fn test_adder_from_string() {
+        let known = Address(0b11, 0b11, 0b11);
+        let unknown = Address::from_string(String::from_str("7"));
     }
 }
