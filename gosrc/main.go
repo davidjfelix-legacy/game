d9f930dfc65bb03af55a266063537cd6617e2e12
David J Felix <felix.davidj@gmail.com>
2015-01-25 19:26:48 +0000

    Add a go demo
    - [skip ci]

---
+++ gosrc/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+    go func() {
+        time.Sleep(10 * time.Second)
+        fmt.Println("First")
+    }()
+
+    go func() {
+        time.Sleep(6 * time.Second)
+        fmt.Println("Second")
+    }()
+
+    go func() {
+        time.Sleep(4 * time.Second)
+        fmt.Println("Third")
+    }()
+
+    go func() {
+        time.Sleep(2 * time.Second)
+        fmt.Println("Fourth")
+    }()
+
+    go func() {
+        time.Sleep(1 * time.Second)
+        fmt.Println("Fifth")
+    }()
+
+    // Eternal Slumber
+    fmt.Println("Main")
+    time.Sleep(20 * time.Second)
+    fmt.Println("Yawn")
+}
