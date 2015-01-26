24039b2bbc849d7a0aea69cc543d779135d90deb
David J Felix <felix.davidj@gmail.com>
2015-01-26 16:23:27 +0000

    [ci skip] Add more heartbeat items

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import redis
 import uuid
 
@@ -6,17 +7,29 @@ redis = redis.StrictRedis(host="localhost", port="6379")
 pid = uuid.uuid4()
 
 @asyncio.coroutine
-def heartbeat(redis, uuid):
+def heartbeat(redis, uuid, work):
     while True:
         yield from asyncio.sleep(2)
-        redis.set("heartbeat-" + str(uuid), "localhost", ex=5)
+        time = datetime.datetime.utcnow().isoformat()
+        pipe = redis.pipeline()
+        pipe.set("server-" + str(uuid), "ip-localhost", ex=5)
+        pipe.set("chunk-" + work, "server-" + str(uuid), ex=5) 
+        pipe.hset("servertimes", "servertime-" + str(uuid), "time-" + time)
+        pipe.hset("serverchunks", "serverchunk-" + str(uuid), "chunk-" + work)
+        pipe.execute()
 
 
 @asyncio.coroutine
 def readbeats(redis):
     while True:
         yield from asyncio.sleep(1)
-        pass
+        now = datetime.datetime.utcnow()
+        pipe = redis.pipeline()
+        pipe.hgetall("servertimes")
+        pipe.hgetall("serverchunks")
+        servertimes, serverchunks = pipe.execute() #FIXME: this is binary return
+        print(str(servertimes))
+        print(str(serverchunks))
 
 @asyncio.coroutine
 def simple_server(reader, writer):
@@ -28,8 +41,8 @@ def simple_server(reader, writer):
 loop = asyncio.get_event_loop()
 tasks = [
     asyncio.start_server(simple_server, '127.0.0.1', 41537, loop=loop),
-    asyncio.async(heartbeat(redis, pid)),
-    asyncio.async(readbeats(""))
+    asyncio.async(heartbeat(redis, pid, "misc")),
+    asyncio.async(readbeats(redis))
 ]
 try:
     loop.run_until_complete(asyncio.wait(tasks))
