a107632a30eb8812469673c4cc9020d903ca7d10
David J Felix <felix.davidj@gmail.com>
2015-05-06 02:38:52 +0000

    Do parallel tick

--- gosrc/main.go
+++ gosrc/main.go
@@ -17,7 +17,11 @@ type Chunk interface {
     tick()
 }
 
-type Entity struct {
+type Entity interface {
+    tick()
+}
+
+type PlayerEntity struct {
     contents Chunk
     velocity Vec3f64
     location Vec3f64
@@ -31,14 +35,25 @@ type Address struct {
     depth uint8
 }
 
+func (a *Address) getTreeCoords() []uint8 {
+    coords := []uint8{}
+    var coord uint8
+    for  i := uint8(0); i < a.depth; i++ {
+        coord = 0 
+        coords = append(coords, coord) 
+    }
+    return coords
+}
+
 type LocalChunk struct {
+    Chunk
     address Address
     center Vec3f64
-    children [8]*Chunk
+    children [8]Chunk
     child_number uint8
-    //entities vec
+    entities []Entity
     mass float64
-    parent *Chunk
+    parent Chunk
     scale uint8
     structure uint64
     summary *Element
@@ -48,11 +63,28 @@ func (l *LocalChunk) addChunk(chunk *Chunk) {
     fmt.Println("adding chunk")
 }
 
+type empty struct {}
+
 func (l *LocalChunk) tick() {
-    fmt.Println("ticking")
+    sem := make(chan empty, len(l.children) + len(l.entities))
+    for _, c := range l.children {
+        go func (c Chunk) {
+            c.tick();
+        }(c);
+    }
+    for _, e := range l.entities {
+        go func (e Entity) {
+            e.tick();
+        }(e);
+    }
+    for i := 0; i < (len(l.children) + len(l.entities)); i++ {
+        <- sem
+    }
+
 }
 
 type RemoteChunk struct {
+    Chunk
     addr string
 }
 
