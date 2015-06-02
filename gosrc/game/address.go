b82f0b4576b592836d3144a598e323d052fa2e24
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-06-02 08:45:59 -0400

    Create address.go

---
+++ gosrc/game/address.go
@@ -0,0 +1,16 @@
+type Address struct {
+    x uint64
+    y uint64
+    z uint64
+    depth uint8
+}
+
+func (a *Address) getTreeCoords() []uint8 {
+    coords := []uint8{}
+    var coord uint8
+    for  i := uint8(0); i < a.depth; i++ {
+        coord = 0 
+        coords = append(coords, coord) 
+    }
+    return coords
+}
