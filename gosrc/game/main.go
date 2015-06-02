48dd8e0caf07111d30c8a8ef728d8dccdd7bf92d
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-06-02 08:47:51 -0400

    Remove address from main.go

--- gosrc/game/main.go
+++ gosrc/game/main.go
@@ -17,26 +17,6 @@ type PlayerEntity struct {
     rotation Vec3f64
 }
 
-type Address struct {
-    x uint64
-    y uint64
-    z uint64
-    depth uint8
-}
-
-func (a *Address) getTreeCoords() []uint8 {
-    coords := []uint8{}
-    var coord uint8
-    for  i := uint8(0); i < a.depth; i++ {
-        coord = 0 
-        coords = append(coords, coord) 
-    }
-    return coords
-}
-
-type empty struct {}
-
-
 func main() {
     dirt := Element{"dirt"}
     world := new(LocalChunk)
