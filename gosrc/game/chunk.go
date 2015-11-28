f11e66120b4546e8c4c3ea71ff972e079796404e
David J Felix <felix.davidj@gmail.com>
2015-11-28 17:44:16 +0000

    All in on this rust thing

--- gosrc/game/chunk.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Chunk interface {
-	addChunk(chunk Chunk)
-	compressChunk(chunk Chunk)
-	removeChunk(chunk Chunk)
-	splitChunk(chunk Chunk)
-	tick()
-}
--- gosrc/game/math.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Vec3f64 struct {
-    x float64
-    y float64
-    z float64
-}
